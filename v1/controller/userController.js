2f9c6ce59988d03fbccd62d718589b571ed7eb56
unknown <Badamasi.link@gmail.com>
2022-03-23 00:15:18 +0100

    added edit user route

--- v1/controller/userController.js
+++ v1/controller/userController.js
@@ -1,6 +1,7 @@
 import * as factory from "./handlerFactory.js";
 
 import User from "../models/userModel.js";
+import AppError from "../utils/appError.js";
 
 export const getUser = factory.getOne(User);
 
@@ -10,4 +11,29 @@ export const getMe = (req, res, next) => {
 	next();
 };
 
-export const editMe = factory.updateOne(User);
+function filterObj(obj, ...allowedFields) {
+	const filteredObj = {};
+	Object.keys(obj).forEach((el) => {
+		if (allowedFields.includes(el)) {
+			filteredObj[el] = obj[el];
+		}
+	});
+	return filteredObj;
+}
+
+export const editMe = async function (req, res, next) {
+	// if (res.body.password || res.body.confirmPassword) {
+	// 	next(AppError("This route is not for changing password.", 400));
+	// }
+	console.log(req);
+	const filteredBody = filterObj(req.body, "name", "email");
+
+	const updatedUser = await User.findByIdAndUpdate(req.user.id, filteredBody, {
+		new: true,
+		runValidators: true,
+	});
+
+	res.status(200).json({ status: "success", data: { user: updatedUser } });
+};
+
+// export const
